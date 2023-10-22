40ba7234ddbbce696f889858326b2394e89851ba
rlatjdgh1122 <98933564+rlatjdgh1122@users.noreply.github.com>
2023-10-23 00:46:52 +0900

    맵 추가

---
+++ Assets/01.Scripts/Agent/Player/PlayerMovement.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float MoveSpeed = 3;
+
+    public void Join_OnMovement(Vector3 value)
+    {
+
+    }
+}
---
+++ Assets/InputSystem/InputReader.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class InputReader : ScriptableObject
+{
+    private InputSystem _inputSystem;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
