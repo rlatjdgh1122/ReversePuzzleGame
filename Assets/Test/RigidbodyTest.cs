4752f6f505458983fa76f7f1551208efde80703e
rlatjdgh1122 <98933564+rlatjdgh1122@users.noreply.github.com>
2023-10-24 11:43:35 +0900

    test

---
+++ Assets/Test/RigidbodyTest.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyTest : MonoBehaviour
+{
+    [Range(0f, 30f)]
+    public float Power = 15f;
+
+    private Rigidbody _rb;
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        //이해를 돕기 위해서 Vector3.right 사용
+        if (Input.GetKeyDown(KeyCode.E))
+            _rb.AddForce(Vector3.right * Power, ForceMode.Impulse);
+    }
+}
